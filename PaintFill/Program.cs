356241e0687f14b1d1676fccb429f6a8051986ba
VegaFromLyra <asha.ab@gmail.com>
2014-02-23 12:20:54 -0800

    Implement the paint fill function that one might see on many image editing programs.
    Given a screen represented by a two dimensional array of colors, a point
    and a new color, fill in the surrounding area until the color changes
    from the original color

---
+++ PaintFill/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintFill
+{
+    enum Color
+    {
+        Black,
+        White,
+        Green,
+        Blue
+    };
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color[,] image = {
+                                 {Color.Black, Color.Black},
+                                 {Color.Black, Color.Black},
+                             };
+
+            int x = 0;
+            int y = 1;
+            int xSize = 2;
+            int ySize = 2;
+
+            Console.WriteLine("Original image is");
+
+            for (int i = 0; i < ySize; i++)
+            {
+                for (int j = 0; j < xSize; j++)
+                {
+                    Console.Write(image[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            if (paintFill(image, x, y, xSize, ySize, Color.Blue))
+            {
+                Console.WriteLine("Painted image is");
+
+                for (int i = 0; i < ySize; i++)
+                {
+                    for (int j = 0; j < xSize; j++)
+                    {
+                        Console.Write(image[i, j] + " ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        // Returns true if paintFill was successful
+        // else returns false
+        static bool paintFill(Color[,] image, int x, int y, int xSize, int ySize, Color newColor)
+        {
+            return paintFill(image, x, y, xSize, ySize, image[y, x], newColor);
+        }
+
+        static bool paintFill(Color[,] image, int x, int y, int xSize, int ySize, Color origColor, Color newColor)
+        {
+            if ((x < 0 || x >= xSize) ||
+                (y < 0 || y >= ySize))
+            {
+                return false;
+            }
+
+            if (image[y, x] == origColor)
+            {
+                image[y, x] = newColor;
+
+                // Go left
+                paintFill(image, x - 1, y, xSize, ySize, origColor, newColor);
+
+                // Go right
+                paintFill(image, x + 1, y, xSize, ySize, origColor, newColor);
+
+                // Go bottom
+                paintFill(image, x, y - 1, xSize, ySize, origColor, newColor);
+
+                // Go top
+                paintFill(image, x, y + 1, xSize, ySize, origColor, newColor);
+            }
+
+            return true;
+        }
+
+    }
+}
